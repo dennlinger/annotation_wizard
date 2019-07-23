27a0572a3a66bced619690a6811fcb27c3d78443
Dennis Aumiller <dennis.aumiller@gmx.de>
2019-07-23 23:40:57 +0200

    Remodeling paragraphs to allow for token-level buttons that are clickable. Top-level App now stores annotation results properly.

--- wizard/src/App.js
+++ wizard/src/App.js
@@ -1,19 +1,59 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import update from 'immutability-helper';
+
+function Token(props) {
+  return (
+      <button className="token" onClick={props.click}>
+        {props.text}
+      </button>
+  );
+}
+
+class Paragraph extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      tokens: props.tokens,
+      click: props.click
+    };
+  }
+  
+  renderToken(val, ind) {
+    return (
+      <Token text={val} click={() => this.state.click(ind)} />
+    )
+  }
+
+  render() {
+    return (
+      <ul>{this.state.tokens.map((val, ind) => {
+        return <li>{this.renderToken(val, ind)}</li>
+      })}</ul>
+    );
+  }
+
+}
+
 
 class App extends Component {
-  state = {
-    isLoaded: false,
-    group: null,
-    sentence: null
-  };
+  constructor(props) {
+    super(props);
+    this.handleClick = this.handleClick.bind(this);
+    this.state = {
+      isLoaded: false,
+      group: null,
+      sentence: null,
+      annotations: null,
+    };
+  }
 
   componentDidMount() {
     this.callBackendAPI()
       .then(res => {
         this.setState({group: res.group,
                        sentence: res.sentence,
+                       annotations: Array(res.sentence.length).fill(0),
                        isLoaded: true});
       })
       .catch(err => console.log(err));
@@ -31,27 +71,21 @@ class App extends Component {
     return body;
   }
 
-  onItemClick = function (event) {
-    event.currentTarget.style.backgroundColor = '#ccc';
+  handleClick(i) {
+    const annotations = this.state.annotations.slice();
+    annotations[i] = 1-annotations[i];
+    this.setState({annotations: annotations});
+    console.log(i);
+    console.log(annotations.slice(0,20));
   }
 
-
   render() {
-    console.log("Hello World");
-    console.log(this.state.sentence);
-    if (this.state.isLoaded) {
-      var items = this.state.sentence.map((item) =>
-        <li onClick={this.onItemClick}>{item}</li>
-      );
-    }
     return (
       <div className="App">
         <p className="group">{this.state.group}</p>
         <div className="sentence">
           {this.state.sentence &&
-          <ul>
-            {items}
-          </ul>
+          <Paragraph tokens={this.state.sentence} click={this.handleClick} />
           }
         </div>
       </div>
