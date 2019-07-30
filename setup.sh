eabdd28d819eead553a039bdb9a3c406c2049a39
Dennis Aumiller <dennis.aumiller@gmail.com>
2019-07-30 18:29:40 +0200

    Now automatically installs NPM packages as well as starts the server.

--- setup.sh
+++ setup.sh
@@ -15,4 +15,10 @@ echo '{"password":"'$4'","counter":0,"annotated":0}' > $1/secrets.json
 # replace port numbers
 sed -i 's/3001/'$2'/g' $1/backend.js
 sed -i 's/3001/'$2'/g' $1/wizard/package.json
-sed -i 's/300/'$frontendPort'/g' $1/wizard/package.json
+sed -i 's/3000/'$frontendPort'/g' $1/wizard/package.json
+npm install
+mkdir -p $1/wizard/node_modules
+npm install --prefix $1/wizard
+
+nohup node backend.js &
+nohup npm start --prefix $1/wizard
